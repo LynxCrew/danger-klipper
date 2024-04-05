c65dd6f5133ee9ec1219720550edab1d5cce3339
Zeanon <thezeanon@gmail.com>
2024-04-05 23:20:12 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -32,6 +32,7 @@ PID_PROFILE_OPTIONS = {
 class Heater:
     def __init__(self, config, sensor):
         self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
         self.config = config
         self.configfile = self.printer.lookup_object("configfile")
         self.name = config.get_name()
