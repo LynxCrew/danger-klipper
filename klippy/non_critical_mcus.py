926c6aff0bfad4962f89378134f1710caec55593
Zeanon <thezeanon@gmail.com>
2024-04-05 22:03:50 +0200

    Update non_critical_mcus.py

--- klippy/non_critical_mcus.py
+++ klippy/non_critical_mcus.py
@@ -4,7 +4,8 @@ class NonCriticalMcus:
 
 
 def add_printer_objects(config):
-    config.get_printer().add_object(
-        "non_critical_mcus",
-        NonCriticalMcus(config.getsection("non_critical_mcus")),
-    )
+    config.get_printer().add_object('non_critical_mcus',
+                                    NonCriticalMcus(
+                                        config
+                                        .getsection('non_critical_mcus')
+                                    ))
