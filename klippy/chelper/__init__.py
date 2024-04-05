641803eee1994c1f09340e0c58894f11d10b8c56
Zeanon <thezeanon@gmail.com>
2024-04-05 20:52:55 +0200

    .

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -13,7 +13,7 @@ import cffi
 
 GCC_CMD = "gcc"
 COMPILE_ARGS = (
-    "-Wall -g -O2 -shared -fPIC"
+    "-Wall -g -O3 -shared -fPIC"
     " -flto -fwhole-program -fno-use-linker-plugin"
     " -march=native -mcpu=native -mtune=native"
     " -o %s %s"
@@ -89,8 +89,7 @@ defs_stepcompress = """
     void steppersync_free(struct steppersync *ss);
     void steppersync_set_time(struct steppersync *ss
         , double time_offset, double mcu_freq);
-    int steppersync_flush(struct steppersync *ss, uint64_t move_clock
-        , uint64_t clear_history_clock);
+    int steppersync_flush(struct steppersync *ss, uint64_t move_clock);
 """
 
 defs_itersolve = """
@@ -124,8 +123,7 @@ defs_trapq = """
         , double start_pos_x, double start_pos_y, double start_pos_z
         , double axes_r_x, double axes_r_y, double axes_r_z
         , double start_v, double cruise_v, double accel);
-    void trapq_finalize_moves(struct trapq *tq, double print_time
-        , double clear_history_time);
+    void trapq_finalize_moves(struct trapq *tq, double print_time);
     void trapq_set_position(struct trapq *tq, double print_time
         , double pos_x, double pos_y, double pos_z);
     int trapq_extract_old(struct trapq *tq, struct pull_move *p, int max
@@ -282,7 +280,6 @@ defs_all = [
     defs_kin_idex,
 ]
 
-
 # Update filenames to an absolute path
 def get_abs_files(srcdir, filelist):
     return [os.path.join(srcdir, fname) for fname in filelist]
@@ -329,7 +326,6 @@ def do_build_code(cmd):
 FFI_main = None
 FFI_lib = None
 pyhelper_logging_callback = None
-
 
 # Hepler invoked from C errorf() code to log errors
 def logging_callback(msg):
