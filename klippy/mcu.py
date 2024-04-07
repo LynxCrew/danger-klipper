0b153298f52b71dad1a457d797ba318c4256b90b
Zeanon <thezeanon@gmail.com>
2024-04-07 16:19:11 +0200

    Update mcu.py

--- klippy/mcu.py
+++ klippy/mcu.py
@@ -853,7 +853,8 @@ class MCU:
             prefix = "Previous MCU '%s' shutdown: " % (self._name,)
 
         append_msgs = []
-        if msg.startswith("ADC out of range"):
+        if (msg.startswith("ADC out of range")
+            and False):
             pheaters = self._printer.lookup_object("heaters")
             heaters = [
                 pheaters.lookup_heater(n) for n in pheaters.available_heaters
