11c6d98f5d4c6e466275354bfc113a31546aad34
Zeanon <thezeanon@gmail.com>
2024-04-10 19:56:54 +0200

    Update z_tilt_ng.py

--- klippy/extras/z_tilt_ng.py
+++ klippy/extras/z_tilt_ng.py
@@ -234,7 +234,7 @@ class ZTilt:
         self.probe_helper.minimum_points(2)
 
         self.z_offsets = config.getlists(
-            "z_offsets", seps="\n", parser=float, count=z_count, default=None
+            "z_offsets", parser=float, count=z_count, default=None
         )
 
         self.z_status = ZAdjustStatus(self.printer)
