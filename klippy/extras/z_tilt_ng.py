941640ac93c916f03e81e86958f2c97317858a38
Zeanon <thezeanon@gmail.com>
2024-04-27 14:27:28 +0200

    Update z_tilt_ng.py

--- klippy/extras/z_tilt_ng.py
+++ klippy/extras/z_tilt_ng.py
@@ -302,7 +302,8 @@ class ZTilt:
                 "No z_positions configured. Run Z_TILT_AUTODETECT first"
             )
             return
-        self.probe_helper.use_xy_offsets(True)
+        if gcmd.get("PROBE_METHOD", "").lower() != "contact":
+            self.probe_helper.use_xy_offsets(True)
         self.z_status.reset()
         self.retry_helper.start(gcmd)
         self.probe_helper.start_probe(gcmd)
