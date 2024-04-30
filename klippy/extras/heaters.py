924e150f4bf539d7e12ac10ca81b7c17b041ffbd
Zeanon <thezeanon@gmail.com>
2024-04-30 12:56:04 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -875,8 +875,7 @@ class ControlVelocityPID:
         self.smoothed_temps.append(
             self.median(self.temps[-self.smoothing_elements:])
         )
-        logging.info("TEMPS_ZEANON")
-        logging.info(self.temps[-self.smoothing_elements:])
+
         self.times.pop(0)
         self.times.append(read_time)
 
