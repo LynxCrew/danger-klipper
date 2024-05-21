7a143b174074a9919c793808b1037ab42d1065dd
Zeanon <thezeanon@gmail.com>
2024-05-21 11:54:03 +0200

    Update heater_fan.py

--- klippy/extras/heater_fan.py
+++ klippy/extras/heater_fan.py
@@ -10,17 +10,28 @@ PIN_MIN_TIME = 0.100
 
 class PrinterHeaterFan:
     def __init__(self, config):
+        self.name = config.get_name().split()[-1]
         self.printer = config.get_printer()
         self.printer.load_object(config, "heaters")
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
         self.heater_names = config.getlist("heater", ("extruder",))
         self.heater_temp = config.getfloat("heater_temp", 50.0)
+        self.heater_temp_off_offset = config.getfloat("heater_temp_off_offset"
+                                                      , 0.0)
         self.heaters = []
         self.fan = fan.Fan(config, default_shutdown_speed=1.0)
-        self.fan_speed = config.getfloat(
-            "fan_speed", 1.0, minval=0.0, maxval=1.0
-        )
+        self.config_fan_speed = config.getfloat("fan_speed",
+                                                1.,
+                                                minval=0.,
+                                                maxval=1.)
+        self.fan_speed = self.config_fan_speed
         self.last_speed = 0.0
+        self.enabled = 1
+        gcode = self.printer.lookup_object('gcode')
+        gcode.register_mux_command(
+            "SET_HEATER_FAN", "HEATER_FAN", self.name,
+            self.cmd_SET_HEATER_FAN,
+            desc=self.cmd_SET_HEATER_FAN_help)
 
     def handle_ready(self):
         pheaters = self.printer.lookup_object("heaters")
@@ -31,20 +42,43 @@ class PrinterHeaterFan:
         )
 
     def get_status(self, eventtime):
-        return self.fan.get_status(eventtime)
+        status = self.fan.get_status(eventtime)
+        status["enabled"] = self.enabled
+        return status
 
     def callback(self, eventtime):
         speed = 0.0
         for heater in self.heaters:
             current_temp, target_temp = heater.get_temp(eventtime)
-            if target_temp or current_temp > self.heater_temp:
+            if ((target_temp or current_temp > self.heater_temp)
+                    or (self.last_speed > 0
+                        and (target_temp or
+                             current_temp > self.heater_temp
+                             - self.heater_temp_off_offset))):
                 speed = self.fan_speed
-        if speed != self.last_speed:
+
+        if self.enabled and speed != self.last_speed:
             self.last_speed = speed
             curtime = self.printer.get_reactor().monotonic()
             print_time = self.fan.get_mcu().estimated_print_time(curtime)
             self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
         return eventtime + 1.0
+
+    cmd_SET_HEATER_FAN_help = "Enable or Disable a heater_fan"
+    def cmd_SET_HEATER_FAN(self, gcmd):
+        self.enabled = gcmd.get_int('ENABLE', self.enabled, minval=0, maxval=1)
+        self.fan_speed = gcmd.get_float('FAN_SPEED',
+                                        self.fan_speed,
+                                        minval=0,
+                                        maxval=1)
+        if self.enabled:
+            curtime = self.printer.get_reactor().monotonic()
+            print_time = self.fan.get_mcu().estimated_print_time(curtime)
+            self.fan.set_speed(print_time + PIN_MIN_TIME, self.fan_speed)
+        if not self.enabled:
+            curtime = self.printer.get_reactor().monotonic()
+            print_time = self.fan.get_mcu().estimated_print_time(curtime)
+            self.fan.set_speed(print_time + PIN_MIN_TIME, 0.0)
 
 
 def load_config_prefix(config):
