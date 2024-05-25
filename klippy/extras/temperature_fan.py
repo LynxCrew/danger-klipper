eac4a5daad9088d8a0f7bb52ae1509b03fe9788c
Zeanon <thezeanon@gmail.com>
2024-05-25 12:36:51 +0200

    Update temperature_fan.py

--- klippy/extras/temperature_fan.py
+++ klippy/extras/temperature_fan.py
@@ -105,7 +105,8 @@ class TemperatureFan:
     def get_status(self, eventtime):
         status = self.fan.get_status(eventtime)
         status["temperature"] = round(self.last_temp, 2)
-        status["target"] = self.target_temp
+        if self.control.get_type != "curve":
+            status["target"] = self.target_temp
         status["control"] = self.control.get_type()
         return status
 
