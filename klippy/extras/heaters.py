24f78c61556ad5d246d5ee78a171f0d2f2fc1281
Zeanon <thezeanon@gmail.com>
2024-05-30 13:39:41 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -431,11 +431,11 @@ class Heater:
                 heater_powers = config_section.getlists(
                     "heater_powers", seps=(",", "\n"), parser=float, count=2, default=None
                 )
-                if heater_power is None and heater_power is None:
+                if heater_power is None and heater_powers is None:
                     raise config_section.error(
                         "Option 'heater_power' or 'heater_powers' "
                         "in section '%s' must be specified"
-                        % config_section.get_name() + " " + name
+                        % (config_section.get_name() + " " + name)
                     )
                 if heater_power is not None and heater_powers is not None:
                     raise config_section.error(
