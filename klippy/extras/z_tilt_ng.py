138cfa39a30e40c290844dd60a3b6f874a30b757
Zeanon <thezeanon@gmail.com>
2024-06-13 18:43:09 +0200

    Update z_tilt_ng.py

--- klippy/extras/z_tilt_ng.py
+++ klippy/extras/z_tilt_ng.py
@@ -560,8 +560,7 @@ class ZTilt:
                                  "were given." % (self.z_count, len(offsets)))
             else:
                 self.z_offsets = offsets
-        gcmd.respond_info("Current z_offsets are: %s" % str(self.z_offsets))
-        gcmd.respond_info(str(type(self.z_offsets)))
+        gcmd.respond_info("Current z_offsets are: %s" % self.z_offsets)
 
 
     def get_status(self, eventtime):
