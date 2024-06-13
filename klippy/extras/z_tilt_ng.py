06005a376a696d5c0885199a464b5a467dfb54de
Zeanon <thezeanon@gmail.com>
2024-06-13 18:25:34 +0200

    Update z_tilt_ng.py

--- klippy/extras/z_tilt_ng.py
+++ klippy/extras/z_tilt_ng.py
@@ -554,6 +554,7 @@ class ZTilt:
             gcmd.respond_error("Offsets have to match amount of z_positions.")
             return
         self.z_offsets = offsets
+        gcmd.respond_info("Current z_offsets are: %s" % str(self.z_offsets))
 
 
     def get_status(self, eventtime):
