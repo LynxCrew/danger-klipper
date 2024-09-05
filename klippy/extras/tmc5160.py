c925066c1d9308cd745a01d974166ccdc23c9c9e
Zeanon <thezeanon@gmail.com>
2024-09-05 12:23:56 +0200

    .

--- klippy/extras/tmc5160.py
+++ klippy/extras/tmc5160.py
@@ -257,7 +257,7 @@ class TMC5160CurrentHelper(tmc.BaseTMCCurrentHelper):
         super().__init__(config, mcu_tmc, MAX_CURRENT)
         pconfig: PrinterConfig = self.printer.lookup_object("configfile")
 
-        self.sense_resistor = config.get("sense_resistor", None, above=0.0)
+        self.sense_resistor = config.getfloat("sense_resistor", None, above=0.0)
         if self.sense_resistor is None:
             pconfig.warn(
                 "config",
