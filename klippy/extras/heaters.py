de92568b62a0d74c23c967798f72d40f43d62e4d
Zeanon <thezeanon@gmail.com>
2024-09-15 15:48:35 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -1254,7 +1254,7 @@ class ControlMPC:
         self.const_filament_density = self.profile["filament_density"]
         self.const_filament_heat_capacity = self.profile["filament_heat_capacity"]
         self.const_maximum_retract = self.profile["maximum_retract"]
-        self.filament_temp_src = self.profile["filament_temp_src"]
+        self.filament_temp_src = self.profile["filament_temperature_source"]
         self.ambient_sensor = self.profile["ambient_temperature_sensor"]
         self.cooling_fan = self.profile["cooling_fan"]
         self.const_fan_ambient_transfer = self.profile["fan_ambient_transfer"]
