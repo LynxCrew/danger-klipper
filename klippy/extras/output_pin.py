60c3e39b2f261c5d684efa85ad017d140939b6b3
Zeanon <thezeanon@gmail.com>
2024-10-04 20:42:17 +0200

    Update output_pin.py

--- klippy/extras/output_pin.py
+++ klippy/extras/output_pin.py
@@ -166,8 +166,6 @@ class PrinterTemplateEvaluator:
                     lparams[p] = ast.literal_eval(v)
                 except ValueError as e:
                     raise gcmd.error("Unable to parse '%s' as a literal" % (v,))
-        gcmd.respond_info(f"{self.active_templates}")
-        gcmd.respond_info(f"{callback}")
         self._activate_template(callback, template, lparams, flush_callback)
         self._activate_timer()
         return tpl_name
