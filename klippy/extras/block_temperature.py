379af42ec4b7e602651d75720f3b3d937b079bc1
Zeanon <thezeanon@gmail.com>
2024-10-09 20:05:28 +0200

    Update block_temperature.py

--- klippy/extras/block_temperature.py
+++ klippy/extras/block_temperature.py
@@ -29,9 +29,9 @@ class MPC_BLOCK_TEMP_WRAPPER:
         )
         self.ignore = self.name in get_danger_options().temp_ignore_limits
 
-        self.printer.register_event_handler("klippy:connect", self._handle_connect)
+        self.printer.register_event_handler("klippy:ready", self._handle_ready)
 
-    def _handle_connect(self):
+    def _handle_ready(self):
         pheaters = self.printer.lookup_object("heaters")
         self.heater = pheaters.lookup_heater(self.heater_name)
         self.temperature_sample_thread.start()
