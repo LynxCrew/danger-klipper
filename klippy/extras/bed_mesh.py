0ee4f15eb5466600fd5785424e75a5d4498f3e62
Zeanon <thezeanon@gmail.com>
2024-10-13 17:18:04 +0200

    Update bed_mesh.py

--- klippy/extras/bed_mesh.py
+++ klippy/extras/bed_mesh.py
@@ -378,10 +378,6 @@ class BedMeshCalibrate:
         self.probe_helper = probe.ProbePointsHelper(
             config, self.probe_finalize, self._get_adjusted_points()
         )
-        self.has_beacon = (
-            config.get_printer().lookup_object("beacon", None) is not None
-            or config.get_printer().load_object(config, "beacon", None) is not None
-        )
         self.scan_speed = None
         if self.radius is None:
             self.scan_speed = config.getfloat(
