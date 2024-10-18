3aebefdc445a30105a0fd9a02e4c06f1af5eafa4
Zeanon <thezeanon@gmail.com>
2024-10-18 20:20:58 +0200

    Update homing.py

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -336,15 +336,6 @@ class Homing:
                     )
             finally:
                 self._set_current_homing(homing_axes, pre_homing=False)
-        if hi.post_retract_dist:
-            self.toolhead.wait_moves()
-            startpos = self._fill_coord(forcepos)
-            homepos = self._fill_coord(movepos)
-            axes_d = [hp - sp for hp, sp in zip(homepos, startpos)]
-            move_d = math.sqrt(sum([d * d for d in axes_d[:3]]))
-            retract_r = min(1.0, hi.post_retract_dist / move_d)
-            retractpos = [hp - ad * retract_r for hp, ad in zip(homepos, axes_d)]
-            self.toolhead.move(retractpos, hi.post_retract_speed)
 
         self._set_current_homing(homing_axes, pre_homing=False)
         # Signal home operation complete
@@ -368,6 +359,15 @@ class Homing:
             for axis in homing_axes:
                 homepos[axis] = newpos[axis]
             self.toolhead.set_position(homepos)
+        if hi.post_retract_dist:
+            self.toolhead.wait_moves()
+            startpos = self._fill_coord(forcepos)
+            homepos = self._fill_coord(movepos)
+            axes_d = [hp - sp for hp, sp in zip(homepos, startpos)]
+            move_d = math.sqrt(sum([d * d for d in axes_d[:3]]))
+            retract_r = min(1.0, hi.post_retract_dist / move_d)
+            retractpos = [hp - ad * retract_r for hp, ad in zip(homepos, axes_d)]
+            self.toolhead.move(retractpos, hi.post_retract_speed)
 
 
 class PrinterHoming:
