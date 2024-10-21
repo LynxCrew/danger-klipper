3305f3f7944adeaf08f986fdd02b6b736ebb5d3a
Zeanon <thezeanon@gmail.com>
2024-10-21 23:05:58 +0200

    Update filament_motion_sensor.py

--- klippy/extras/filament_motion_sensor.py
+++ klippy/extras/filament_motion_sensor.py
@@ -3,8 +3,6 @@
 # Copyright (C) 2021 Joshua Wherrett <thejoshw.code@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
-
 from . import filament_switch_sensor
 
 
