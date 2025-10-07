b4dba690eac68f067d2cedd2a94a811bfe4f28fc
Zeanon <thezeanon@gmail.com>
2025-10-07 15:45:13 +0200

    Update homing.py

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -503,10 +503,7 @@ class Homing:
         else:
             for i in range(3):
                 pos[i] += self._calc_mean([dist[i] for dist in distances])
-
-        gcode.respond_info(f"{pos}")
-        gcode.respond_info(f"Homing sample for {['XYZ'[i] for i in homing_axes]}: {[pos[i] for i in homing_axes]}")
-
+        gcode.respond_info(f"Final homing position for {['XYZ'[i] for i in homing_axes]}: {[pos[i] for i in homing_axes]}")
         self.toolhead.set_position(pos)
 
         self.adjust_pos = {}
