259b040b7423962fa37d8985bc074df0d082a011
Zeanon <thezeanon@gmail.com>
2025-10-08 16:55:13 +0200

    Update homing.py

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -487,7 +487,8 @@ class Homing:
 
         if len(distances) > 1:
             self.toolhead.wait_moves()
-            pos = home_pos = list(self.toolhead.get_position())
+            pos = self.toolhead.get_position()
+            home_pos = self.toolhead.get_position()
             self.gcode.respond_info(f"{home_pos}")
             calc_adjustment = (
                 self._calc_median
