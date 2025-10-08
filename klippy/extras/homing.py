0812415cbfa199cd156580ffccef6d1ea8319380
Zeanon <thezeanon@gmail.com>
2025-10-08 16:45:46 +0200

    Update homing.py

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -505,6 +505,7 @@ class Homing:
                 )
             self.toolhead.set_position(pos)
             if hi.move_toolhead_after_adjusting:
+                self.printer.lookup_object("gcode_move").last_position = home_pos
                 self.toolhead.move(home_pos, hi.retract_speed)
 
         self.adjust_pos = {}
